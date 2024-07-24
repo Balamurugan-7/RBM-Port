8cafbc7aac5440c9ce69f15f3a9f7829308016da
Balamurugan <160770970+Balamurugan-7@users.noreply.github.com>
2024-07-24 15:36:35 +0530

    Update Log.js

--- client/src/Pages/Log.js
+++ client/src/Pages/Log.js
@@ -24,7 +24,7 @@ function Log() {
     }
     console.log('Logging in with:', { name, email });
 
-    axios.post('https://rbm-port-backend.onrender.com', { name, email })
+    axios.post('https://rbm-port-backend.onrender.com/', { name, email })
       .then(() => {
         navigate('/Balamurugan');
       })
@@ -34,7 +34,7 @@ function Log() {
   }
 
   function go() {
-    axios.post('https://rbm-port-backend.onrender.com', { name, email })
+    axios.post('https://rbm-port-backend.onrender.com/', { name, email })
       .then(() => {
         navigate('/Balamurugan');
       })
